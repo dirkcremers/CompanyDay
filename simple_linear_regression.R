0e94562b6bf94cd45792cd04964d5aee221d7e8f
bmmarsman <bmmarsman@gmail.com>
2019-04-30 16:02:23 +0200

    joejoe

--- simple_linear_regression.R
+++ simple_linear_regression.R
@@ -36,7 +36,7 @@ ggplot() +
   ggtitle('Salary vs Experience (Training set)') +
   xlab('Years of experience') +
   ylab('Salary')
-
+print(joejoe)
 # Visualising the Test set results
 library(ggplot)
 ggplot() +
